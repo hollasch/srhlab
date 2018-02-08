41f8d00e23919b3e07d005585cd8298572cee72e
Steve Hollasch <steve@hollasch.net>
2018-02-08 03:16:01 -0800

    Add C# experiments

---
+++ cs/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Hello from my world!");
+        }
+    }
+}
