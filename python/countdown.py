37d6509bfe6aee3628126f81331a6f1582e74842
Steve Hollasch <steve@hollasch.net>
2018-03-21 20:52:51 -0700

    python/countdown.py now takes command-line arg

--- python/countdown.py
+++ python/countdown.py
@@ -3,12 +3,14 @@
 import time
 import sys
 
+seconds_remaining = int(sys.argv[1]) if (len(sys.argv) > 1) else 75
+
 def countdown(secs):
     while (secs > 0):
         sys.stdout.write('\r%02d:%02d ' % (secs/60,secs%60))
         sys.stdout.flush()
         time.sleep(1)
         secs -= 1
-    print '\rDone.'
+    print ('\rDone.')
 
-countdown(75)
+countdown(seconds_remaining)
