0fa4a1621c94eff4488f504b8f998f4ce8d8e2ee
Steve Hollasch <steve@hollasch.net>
2018-03-21 20:36:46 -0700

    Update cmdargs.py

--- python/cmdargs.py
+++ python/cmdargs.py
@@ -2,8 +2,11 @@
 
 import sys
 
+num_args = len(sys.argv)
+print ("{0} argument{1}" . format(num_args, "s" if (num_args != 1) else ""))
+
 index = 0
 
-for arg in sys.argv[0:]:
-    print ("sys.argv[{0}]: {1}".format(index,arg))
+for arg in sys.argv:
+    print ("sys.argv[{0}]: \"{1}\"" . format(index,arg))
     index += 1
