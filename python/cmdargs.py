7183515296c15dc5abafc1b80e13663510ab879d
Steve Hollasch <steve@hollasch.net>
2018-04-10 13:26:15 -0700

    Python - use standard __main__ pattern

--- python/cmdargs.py
+++ python/cmdargs.py
@@ -2,11 +2,13 @@
 
 import sys
 
-num_args = len(sys.argv)
-print ("{0} argument{1}" . format(num_args, "s" if (num_args != 1) else ""))
 
-index = 0
+if __name__ == "__main__":
+    num_args = len(sys.argv)
+    print ("{0} argument{1}" . format(num_args, "s" if (num_args != 1) else ""))
 
-for arg in sys.argv:
-    print ("sys.argv[{0}]: \"{1}\"" . format(index,arg))
-    index += 1
+    index = 0
+
+    for arg in sys.argv:
+        print ("sys.argv[{0}]: \"{1}\"" . format(index,arg))
+        index += 1
