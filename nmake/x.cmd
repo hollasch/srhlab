d5135fcb1864a611f9653392b5cc945b1e737993
Steve Hollasch <steve@hollasch.net>
2018-09-16 14:14:23 -0700

    Add nmake lab from private repo

---
+++ nmake/x.cmd
@@ -0,0 +1,1 @@
+set mlist=d1\makefile d2\makefile d1\d1a\makefile d1\d1b\makefile d1\d1a\d1a1\makefile d1\d1a\d1a2\makefile d1\d1b\d1b1\makefile d1\d1b\d1b2\makefile d1\d1b\d1b3\makefile d2\d2a\makefile
