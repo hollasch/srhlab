1a04650e74c212c2a17575dddb83c6bf11e91412
Steve Hollasch <steve@hollasch.net>
2020-11-12 13:30:34 -0800

    cpp: add tuple

---
+++ cpp/tuple.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+tuple<bool,char,double,int> f()
+{
+    return { true, 'q', 3.7, 11 };
+}
+
+void report(bool b, char c, double d, int i) {
+    cout << "b,c,d,i = {"
+         << (b ? "true" : "false") << ", "
+         << '\'' << c << "', "
+         << d << ", "
+         << i
+         << "}\n";
+}
+
+void main()
+{
+    // Create an immediate tuple and access via std::get<#>(var).
+
+    auto x = std::make_tuple (true, 'q', 3.7, 11);
+
+    cout << "x = {"
+         << (std::get<0>(x) ? "true" : "false") << ", "
+         << '\'' << std::get<1>(x) << "', "
+         << std::get<2>(x) << ", "
+         << std::get<3>(x)
+         << "}\n";
+
+    // Apply a tuple to a function.
+
+    std::apply(report, x);
+
+    // Use std::tie to destructure a tuple into individual variables.
+
+    bool   b;
+    char   c = '-';
+    double d;
+    int    i;
+
+    std::tie(b,std::ignore,d,i) = f();
+
+    cout << "b,c,d,i = {"
+         << (b ? "true" : "false") << ", "
+         << '\'' << c << "', "
+         << d << ", "
+         << i
+         << "}\n";
+}
