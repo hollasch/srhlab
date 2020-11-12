cc9074bf33962c73ba439e02f8e31e8d73904ad1
Steve Hollasch <steve@hollasch.net>
2020-11-12 13:02:38 -0800

    cpp pair: add std::tie example

--- cpp/pair.cpp
+++ cpp/pair.cpp
@@ -11,4 +11,9 @@ void main()
 {
     auto x = f();
     cout << "x.first = " << x.first << ", x.second = " << (x.second ? "true" : "false") << "\n";
+
+    double a;
+    bool b;
+    std::tie(a,b) = f();
+    cout << "a = " << a << ", b = " << (b ? "true" : "false") << "\n";
 }
