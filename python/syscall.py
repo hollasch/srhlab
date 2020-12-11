c0b23368abc4b40ac5e434c9cbeb4a679a5e7b0f
Steve Hollasch <steve@hollasch.net>
2020-12-11 15:23:17 -0800

    wip: python/syscall

--- python/syscall.py
+++ python/syscall.py
@@ -1,5 +1,30 @@
+
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# WARNING -- still to be worked out. In particular, there are Python2 vs Python3 issues below with `subprocess`.
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
 # Launching subprocesses
 
+import os
 import subprocess
 
+print 'Uh oh. This is Python 2.\n'
+
+# Simple call
+print("Printing UTC time (depends on 'timeprint').")
 subprocess.call("timeprint -z UTC \"Current time is %H:%M:%S UTC\"")
+
+# Executing a command and checking status. Useful for simple command-line scripts much like batch files.
+cmd = 'dir bogus-dir\\bogus-file'
+print("\nCalling os.system('{}').".format(cmd))
+
+result = os.system(cmd)
+print("Command returned {}.".format(result))
+
+# Executing a command and processing the single-line output
+cmd = ['findstr', '-c:"env"', 'README.md']
+print("\nRunning '{}'.".format(cmd))
+output = subprocess.run(cmd)
+print("Command output is '{}'.".format(output.decode("utf-8")))
