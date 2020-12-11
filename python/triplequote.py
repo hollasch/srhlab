439eff74cbc34e06e2cfaa222c1097f5d54a5ea1
Steve Hollasch <steve@hollasch.net>
2020-12-11 14:01:01 -0800

    tripletick.py -> triplequote.py

---
+++ python/triplequote.py
@@ -0,0 +1,27 @@
+# Triple-quoted strings
+
+str = '''
+Hello
+There
+'''
+
+print(str)
+
+print('<' '''
+    Hello
+    There
+''' '>')
+
+'''
+    Triple quoted strings are
+    defined but then discarded,
+    so they effectively function
+    as comment blocks in many
+    Python scripts
+'''
+
+"""
+    Note that you can use triple
+    double-quotes for strings as
+    well.
+"""
