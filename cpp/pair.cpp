f3593f4f04a48a2a29d09a98fa71be0043ad98d9
Steve Hollasch <steve@hollasch.net>
2022-11-07 18:17:29 -0800

    cpp/pair: add structured binding example for loop

--- cpp/pair.cpp
+++ cpp/pair.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -12,17 +13,32 @@ void report(bool b, double d) {
 
 int main() {
     // Get pair return from function.
+    cout << "\n# Pair Return Values\n";
     auto x = f();
     report(x.first, x.second);
 
     // Function application
+    cout << "\n# Function Application\n";
     std::apply(report, x);
 
     // Destructuring
+    cout << "\n# Destructuring\n";
     bool b;
     double d;
     std::tie(b,d) = f();
     report(b,d);
+
+    cout << "\n# Structured Bindings\n";
+
+    vector<pair<int, string>> pairs {
+        { 0, "red" },
+        { 1, "green" },
+        { 2, "blue" }
+    };
+
+    for (auto [key, desc] : pairs) {
+        cout << "int(" << key << ") = \"" << desc << "\"\n";
+    }
 
     return 0;
 }
