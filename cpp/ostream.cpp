51c92abba242e0c48ce90a9d68e9936228213e98
Steve Hollasch <steve@hollasch.net>
2023-12-12 12:35:25 -0800

    cpp/ostream: output section comment

--- cpp/ostream.cpp
+++ cpp/ostream.cpp
@@ -29,23 +29,26 @@ std::ostream& operator<<(std::ostream& out, const Color& color) {
 
 int main() {
 
-    // setfill and setw only apply to the immediately following non-manipulator output item.
+    std::cout << "\nsetfill and setw only apply to the immediately following non-manipulator output item.\n";
     std::cout << std::setfill('+') << std::setw(20) << 37 << '\n';
-    std::cout << 37 << '\n';
+    std::cout << 37 << "\n";
     std::cout << std::setfill('+') << std::setw(20) << 37 << 45 << '\n';
     std::cout << '[' << std::setfill('+') << std::setw(20) << 37 << ']'
               << '[' << std::setfill('+') << std::setw(20) << 37 << ']' << '\n';
     std::cout << '[' << std::setfill('+') << std::setw(20) << 37 << ']' << '[' << 37 << ']' << '\n';
 
-    // hex/dec modifiers persist until set to something else.
-    std::cout << '\n';
+    std::cout << "\nHex output with leading zeros.\n";
+    std::cout << "Hex 1: " << std::setfill('0') << std::setw(8) << std::hex << 1 << "\n";
+    std::cout << "Hex 1 long: " << std::setfill('0') << std::setw(16) << std::hex << 0x123456789aL << "\n";
+
+    std::cout << "\nhex/dec modifiers persist until set to something else.\n";
     std::cout << std::hex << 37 << '\n';   // 25
     std::cout << 37 << '\n';          // 25
     std::cout << std::dec << 37 << '\n';   // 37
 
+    std::cout << "\nExample using 'std::ostream& print()' member function.\n";
     auto c = Color(0.125, 0.25, 0.618033988749895);
-
-    std::cout << '\n' << "color: " << c << "\n"; // "color: [0.1250, 0.5000, 0.6180]"
+    std::cout << "color: " << c << "\n"; // "color: [0.1250, 0.5000, 0.6180]"
 
     return 0;
 }
